0d31f09f026165196498dab238e58d8de88accf0
Richard <richard@dqubeglobal.com>
2018-02-10 08:38:42 +0530

    Some changes including mailing options started

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,58 @@
 
+/* Contact Form
+    * ------------------------------------------------------ */
+    var clContactForm = function() {
+        
+        /* local validation */
+        $('#contactForm').validate({
+        
+            /* submit via ajax */
+            submitHandler: function(form) {
+    
+                var sLoader = $('.submit-loader');
+    
+                $.ajax({
+    
+                    type: "POST",
+                    url: "mailing/sendEmail.php",
+                    data: $(form).serialize(),
+                    beforeSend: function() { 
+						
+                        sLoader.slideDown("slow");
+    
+                    },
+                    success: function(msg) {
+    
+                        // Message was sent
+                        if (msg == 'OK') {
+                            sLoader.slideUp("slow"); 
+                            $('.message-error').fadeOut();
+                            $('#contactForm').fadeOut();
+                            $('.message-success').fadeIn();
+                        }
+                        // There was an error
+                        else {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').html(msg);
+                            $('.message-warning').slideDown("slow");
+                        }
+                    },
+                    error: function() {
+/*
+    
+                        sLoader.slideUp("slow"); 
+*/                      sLoader.slideUp("slow");
+                        $('.message-warning').html("Something went wrong. Please try again.");
+                        $('.message-warning').slideDown("slow");
+                    }
+
+                });                
+				   /* $('#ResponceModal').modal("show");*/
+            }
+    
+        });
+    };
+
 $(document).ready(function(){
 
 
